8b2cc9337eec71f578f97c3fa24d12694741bd90
DNMetodiev <metodievdenis@yahoo.com>
2024-05-22 23:51:32 +0300

    add cells dynamically + commented out hardcoded cells

--- pathfinder-maze/src/App.jsx
+++ pathfinder-maze/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let maze = [
+    ['wall', 'wall', 'wall', 'wall'],
+    ['start', 'path', 'path', 'wall'],
+    ['wall', 'wall', 'path', 'wall'],
+    ['wall', 'wall', 'path', 'end']
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {maze.map((row, rowIndex) => (
+        <div className="row">
+          {row.map((cell, cellIndex) => (
+            <div className={`cell ${cell}`}></div>
+          ))}
+        </div>
+      ))}
+      {/* <div className={"row"}>
+        <div className={"cell wall"}></div>
+        <div className={"cell path"}></div>
+        <div className={"cell start"}></div>
+        <div className={"cell end"}></div>
+      </div> */}
+    </div>
   )
 }
 
